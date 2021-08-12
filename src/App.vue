8fe11996268966d584789334abde7cfe10466dba
Samir Shrestha <theonlysamir@gmail.com>
2021-08-13 00:06:51 +0545

    Bootstrap and fontawesome added

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,40 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
     <router-view/>
   </div>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+@import url("https://fonts.googleapis.com/css2?family=Raleway:wght@200;300;400;500;600;700&display=swap");
+
+body {
+  font-family: 'Raleway',
+ sans-serif;
+  font-smooth: antialiased;
+  font-weight: 500;
 }
 
-#nav {
-  padding: 30px;
+body p {
+  color: #424242;
+  line-height: 28px;
+  font-size: 14px;
+}
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+.f-s1 {
+  text-decoration: none;
+  color: black;
+  font-size: 24px;
+}
 
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+.f-s2 {
+  text-decoration: none;
+  color: black;
+  font-size: 18px;
+}
+
+.f-s3 {
+  text-decoration: none;
+  color: black;
+  font-size: 14px;
 }
 </style>
